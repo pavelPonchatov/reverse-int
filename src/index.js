7a36051b1a813a2c6f5a124c3a09e978d1dc0637
pavel <pavel.pronchatov155@gmail.com>
2020-02-20 00:16:39 +0400

    feat: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    let arr = String(n).split("");
+    console.log(arr);
+    if(arr[0] == "-"){
+        arr.splice(arr[0],1);
+    }
   
+    return Number(arr.reverse().join(""));
 }
